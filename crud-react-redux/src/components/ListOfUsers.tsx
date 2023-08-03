26c371f70d3d09a6d839cbd5b5020492e04fac5a
Alexander Ramírez Chiquito <alexander.ramirezc@unac.edu.co>
2023-08-03 06:54:52 -0500

    incio

---
+++ crud-react-redux/src/components/ListOfUsers.tsx
@@ -0,0 +1,114 @@
+import {
+  Card,
+  Table,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableHeaderCell,
+  TableBody,
+  Title,
+  Badge,
+} from "@tremor/react";
+
+const users: {
+  id: string;
+  name: string;
+  email: string;
+  github: string;
+}[] = [
+  {
+    id: "1",
+    name: "Peter Gomez",
+    email: "peter@gmail.com",
+    github: "peterDoe",
+  },
+  {
+    id: "2",
+    name: "Alexander chisquis",
+    email: "chikis@gmail.com",
+    github: "cikisA",
+  },
+  {
+    id: "3",
+    name: "Shar Gomez",
+    email: "gomez@gmail.com",
+    github: "sharGomez",
+  },
+];
+
+export function ListOfUsers() {
+  return (
+    <Card>
+      <Title>
+        Usuarios:
+        <Badge style={{ marginLeft: "8px" }}>{users.length}</Badge>
+      </Title>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableHeaderCell>Id</TableHeaderCell>
+            <TableHeaderCell>Nombre</TableHeaderCell>
+            <TableHeaderCell>Email</TableHeaderCell>
+            <TableHeaderCell>Acciones</TableHeaderCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {users.map((item) => (
+            <TableRow key={item.name}>
+              <TableCell>{item.id}</TableCell>
+              <TableCell style={{ display: "flex", alignItems: "center" }}>
+                <img
+                  style={{
+                    width: "32px",
+                    height: "32px",
+                    borderRadius: "50%",
+                    marginRight: "8px",
+                  }}
+                  src={`https://unavatar.io/github/${item.github}`}
+                  alt={item.name}
+                />
+                {item.name}
+              </TableCell>
+              <TableCell>{item.email}</TableCell>
+              <TableCell>
+                <button type="button">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                    />
+                  </svg>
+                </button>
+                <button type="button">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m6 4.125l2.25 2.25m0 0l2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                    />
+                  </svg>
+                </button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Card>
+  );
+}
